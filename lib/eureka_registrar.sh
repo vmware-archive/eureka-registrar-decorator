c7a3df00f80d311d9c3308eb68563eb14894c552
guidowb <guidowb@gmail.com>
2017-02-22 12:20:16 -0600

    Initial open source release

---
+++ lib/eureka_registrar.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# eureka-registrar-decorator
+#
+# Copyright (c) 2017-Present Pivotal Software, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# The *-config.py program will directly manipulate things like
+# configuration files. But it can not directly manipulate the
+# environment variables, as that would only affect its process
+# hierarchy. So for environment variables, we have it provide
+# us with the keys and values on its output stream, and we place
+# them in exported environment variables, here - in the parent
+# process - which will also be the parent for the application.
+
+python eureka_registrar.py &
